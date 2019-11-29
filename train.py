00c02dcfd1f15ab8e2e6124a1d515bd1ea8bca5c
kerry <hyounguk1112@gmail.com>
2019-11-29 16:08:28 +0900

    update

--- train.py
+++ train.py
@@ -21,17 +21,17 @@ ds = tf.data.Dataset.from_tensor_slices(
     (image_paths, bbox, joints)).shuffle(10000)
 
 
-def _fixup_shape(cropped_img, heatmap):
+def fixup_shape(cropped_img, heatmap):
     cropped_img.set_shape([None, 256, 192, 3])
     heatmap.set_shape([None, 64, 48, 17])  # I have 17 classes
     return cropped_img, heatmap
 def data_process(file_path, bbox, joints):
     [cropped_img, target_coord] = tf.py_function(image_process.cropped_image_and_pose_coord, [file_path, bbox, joints], [tf.float32, tf.int16])
     heatmap = image_process.render_gaussian_heatmap(target_coord, config.output_shape)
-    return cropped_img/255., heatmap
+    return fixup_shape(cropped_img/255., heatmap)
 
 
-processed_ds = ds.map(data_process).batch(64).map(_fixup_shape)
+processed_ds = ds.map(data_process).batch(64)
 pose_estimator = tf.keras.models.load_model('model/pose.h5')
 optimizer = tf.keras.optimizers.Adam(0.001)
 pose_estimator.compile(loss='mse',
