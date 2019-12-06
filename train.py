f3f8b132b5bd798b51eb40fb78a6ee73de6074c0
kerry <hyounguk1112@gmail.com>
2019-12-06 10:37:23 +0900

    update

--- train.py
+++ train.py
@@ -5,13 +5,13 @@ import numpy as np
 import cv2
 from data_util.COCO import dataset, image_process
 import tensorflow as tf
-import tensorflow_addons as tfa
 from matplotlib import pyplot as plt
 from config.coco_config import config
 
 config = config()
 dbcfg = dataset.Dataset()
 train_data = dbcfg.load_train_data()
+val_data =dbcfg.load_val_data_with_annot()
 
 image_paths = [i['imgpath'] for i in train_data]
 bbox = [i['bbox'] for i in train_data]
@@ -20,10 +20,17 @@ joints = [i['joints'] for i in train_data]
 ds = tf.data.Dataset.from_tensor_slices(
     (image_paths, bbox, joints)).shuffle(10000)
 
+image_paths = [i['imgpath'] for i in val_data]
+bbox = [i['bbox'] for i in val_data]
+joints = [i['joints'] for i in val_data]
+
+val_ds = tf.data.Dataset.from_tensor_slices(
+    (image_paths, bbox, joints))
+
 
 def fixup_shape(cropped_img, heatmap):
-    cropped_img.set_shape([None, 256, 192, 3])
-    heatmap.set_shape([None, 64, 48, 17])  # I have 17 classes
+    cropped_img.set_shape([256, 192, 3])
+    heatmap.set_shape([64, 48, 17])  # I have 17 classes
     return cropped_img, heatmap
 def data_process(file_path, bbox, joints):
     [cropped_img, target_coord] = tf.py_function(image_process.cropped_image_and_pose_coord, [file_path, bbox, joints], [tf.float32, tf.int16])
@@ -32,6 +39,7 @@ def data_process(file_path, bbox, joints):
 
 
 processed_ds = ds.map(data_process).batch(64)
+processed_val_ds = ds.map(data_process).batch(64)
 pose_estimator = tf.keras.models.load_model('model/pose.h5')
 optimizer = tf.keras.optimizers.Adam(0.001)
 pose_estimator.compile(loss='mse',
@@ -42,4 +50,7 @@ EPOCHS = 120
 
 history = pose_estimator.fit(
     processed_ds,
+    validation_data=processed_val_ds
     epochs=EPOCHS)
+
+pose_estimator.save('trained.h5')
